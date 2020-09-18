0e7abbd59eb3f032c6fe1f0eae141e2b9792fd85
lekhanhe16ptit <lekhanhe16ptit@gmail.com>
2020-09-18 10:40:37 +0700

    ud

--- DoAnSTP-AppMobile/app/src/main/java/com/kl/doanstp/adapter/MatchControlDataLoad.java
+++
@@ -1,9 +0,0 @@
-package com.kl.doanstp.adapter;
-
-import com.kl.doanstp.model.Player;
-import com.kl.doanstp.model.Team;
-
-public interface MatchControlDataLoad {
-    Team onGetTeamData();
-    Player onGetPlayerData();
-}
--- DoAnSTP-AppMobile/app/src/main/java/com/kl/doanstp/service/ConnectionUtil.java
+++
@@ -1,6 +0,0 @@
-package com.kl.doanstp.service;
-
-public class ConnectionUtil {
-    public static String baseURL = "http://192.168.0.105:5000";
-
-}
